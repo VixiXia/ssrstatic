3ca052cfc8f17c6aa9af6576eb133ace7e8d45e2
VixiXia <victoriaxia@outlook.com>
2026-07-09 11:11:37 +0800

    ff

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import { unstable_noStore as noStore } from 'next/cache';
 
 export default function Home() {
+  noStore();
+
+  const timeOnServer = new Date().toLocaleTimeString('en-US');
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -8,6 +12,9 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <div>
+           This is a Next.js application hosted on Azure Static Web Apps with hybrid rendering. The time on the server is <strong>{timeOnServer}</strong>.
+        </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
